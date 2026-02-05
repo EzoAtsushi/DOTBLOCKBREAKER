dcbb2961fb6d5fc70d469b16ac72f673fa35b41c
EzoAtsushi <atsushi.takeda.76@gmail.com>
2026-02-05 10:10:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+/* Block Laser Breaker PWA SW */
+const CACHE_NAME = "blb-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192x192.png",
+  "./icon-512x512.png",
+  "./icon-180x180.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  event.respondWith(
+    caches.match(req)
+      .then((cached) => cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => cached))
+  );
+});
